0f397bddba807a11433a71e2a58a667f27a36fa3
nulldeath <krawdev@gmail.com>
2023-07-12 17:57:36 -0300

    service implement

--- src/main/java/com/nulldeath/apimongo/service/StudentServiceImp.java
+++ src/main/java/com/nulldeath/apimongo/service/StudentServiceImp.java
@@ -2,19 +2,59 @@ package com.nulldeath.apimongo.service;
 
 import com.nulldeath.apimongo.model.StudentRequest;
 import com.nulldeath.apimongo.model.StudentResponse;
+import com.nulldeath.apimongo.persistence.entity.Student;
+import com.nulldeath.apimongo.persistence.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentServiceImp implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Override
     public StudentResponse create(StudentRequest request) {
-        return null;
+        Student student = new Student();
+        student.setName(request.getName());
+        student.setBirthDate(request.getBirthDate());
+        student.setDocument(request.getDocument());
+
+        studentRepository.save(student);
+
+        return createResponse(student);
+
+    }
+
+    private StudentResponse createResponse(Student student) {
+
+        StudentResponse response = new StudentResponse();
+        response.setId(student.getId());
+        response.setName(student.getName());
+        response.setDocument(student.getDocument());
+        response.setBirthDate(student.getBirthDate());
+
+        return response;
     }
 
     @Override
     public List<StudentResponse> getAll() {
-        return null;
+        List<StudentResponse> responses = new ArrayList<>();
+
+        List<Student> students = studentRepository.findAll();
+
+        if(!students.isEmpty()) {
+            students.forEach(student -> responses.add(createResponse(student)));
+            
+        }
+
+        for (Student student : students) {
+            responses.add(createResponse(student));
+        }
+
+        return responses;
     }
 }
